5a92673e436cb383082e5d59e1fe7cb9ad5d01e0
DevMatheusMonteiro <carlosmathmonteiro@gmail.com>
2023-09-29 17:02:05 -0300

    add animate

--- script.js
+++ script.js
@@ -7,4 +7,14 @@ toggleNavList.onclick = () => {
   span.textContent = navList.classList.toggle("hide")
     ? "Abrir menu de navegação"
     : "Fechar menu de navegação";
+
+  navList.animate(
+    [
+      { transform: "translateY(0%)", opacity: "0" },
+      { transform: "translateY(100%)" },
+    ],
+    {
+      duration: 300,
+    }
+  );
 };
